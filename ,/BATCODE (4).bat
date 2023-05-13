385d97589677e78963195ca9e529990c8c8ec06d
banditblood <133361802+banditblood@users.noreply.github.com>
2023-05-13 16:40:08 +0400

    Delete , directory

--- ,/BATCODE (4).bat
+++
@@ -1,6 +0,0 @@
-color a
-@echo off
-:loop
-arp -a
-timeout /t 5 /nobreak > nul
-goto loop
--- ,/BATCODE (6).bat
+++
@@ -1,7 +0,0 @@
-color a
-@echo off
-:loop
-ipconfig /release
-ipconfig /renew
-timeout /t 3 /nobreak > nul
-goto loop
